107f633afaa72eae1bbcaac5c9696ec66a9ed123
Seamus Connolly <sghconnolly@gmail.com>
2018-03-08 09:29:17 +0000

    Add Enemy constructor function.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    constructor() {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+    }
 
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
